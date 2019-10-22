f268f80441e5b2a8cdad09faa7f82299631f0c03
voyccom <john@hagstrand.com>
2019-10-22 03:16:03 -0700

    put thai dict in db

--- sql/create_mai_schema.sql
+++ sql/create_mai_schema.sql
@@ -20,3 +20,32 @@ create table mai.vocab (
 	recency bigint,
 	mastery integer
 );
+
+create table mai.thaidict (
+	id serial primary key,
+	type char(1),        /* g */ 
+	thai varchar(100),   /* t */
+	source int,          /* s */
+	level int,           /* l */
+	numdef int,          /* n */
+	pos char(1),         /* p */
+	eng varchar(100),    /* e */
+	details varchar(500),/* d */
+	
+	/* glyph */
+	unicode char(6),     /* u */
+	reference char(1),   /* r */
+	class char(1),       /* m */
+	subclass char(1),    /* a */
+	
+	/* syllable and one-syllable word */
+	leadingconsonant varchar(5),
+	finalconsonant varchar(5),
+	vowelpattern varchar(10),
+	tone char(1),
+	rules varchar(100),
+	
+	/* multi-syllable word and phrase */
+	numsyllables int,
+	components varchar(500)
+);
