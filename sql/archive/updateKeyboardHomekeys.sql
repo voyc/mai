df3727559f020e13855cf9dea0652ef34647f720
voyccom <john@hagstrand.com>
2019-11-16 22:24:03 -0700

    update homekey words

---
+++ sql/archive/updateKeyboardHomekeys.sql
@@ -0,0 +1,8 @@
+update mai.thaidict set vowelpattern='o', leadingconsonant='หก', finalconsonant='', tonemark='', tone='L', translit='go', rules='ccivo,cclh,ovs,hcd' where id = 1429;
+update mai.thaidict set vowelpattern='oา', leadingconsonant='ส', finalconsonant='ว', tonemark='', tone='R', translit='saao', rules='fsc,hcl' where id = 1428;
+update mai.thaidict set vowelpattern='oา', leadingconsonant='ด', finalconsonant='ว', tonemark='', tone='M', translit='daao', rules='fsc,mcl' where id = 1425;
+update mai.thaidict set vowelpattern='oา', leadingconsonant='ว', finalconsonant='', tonemark='่', tone='F', translit='waa', rules='ovl,lc1' where id = 1434;
+update mai.thaidict set vowelpattern='o', leadingconsonant='ส', finalconsonant='ด', tonemark='', tone='L', translit='sot', rules='ccivo,fnsc,hcd' where id = 1427;
+update mai.thaidict set vowelpattern='oา', leadingconsonant='ก', finalconsonant='', tonemark='', tone='M', translit='gaa', rules='ovl,mcl' where id = 165;
+update mai.thaidict set vowelpattern='oา', leadingconsonant='ห', finalconsonant='', tonemark='', tone='R', translit='haa', rules='ovl,hcl' where id = 1430;
+update mai.thaidict set vowelpattern='oา', leadingconsonant='กว', finalconsonant='', tonemark='่', tone='L', translit='gwaa', rules='ovl,mc1' where id = 1424;
