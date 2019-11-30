6794697b82cc43870b36647b96b183ecb4d73aa6
John <john@hagstrand.com>
2019-11-30 12:25:41 +0700

    add images

---
+++ html/thai/course/encyclopedia/flowers.js
@@ -0,0 +1,51 @@
+ดอก
+กบัว
+Lotus
+
+ราดชะพรึก
+Golden shower tree.  Official national flower of Thailand, and of the King of Thailand.
+
+เปลวไฟ
+Flame Tree, Delonix Regia, bloom in May
+
+มะลิ
+Jasmine
+
+กล้วยไม้
+orchid
+
+ซากูระ
+Sakura, Japanese flowering cherry tree
+
+ลีลาวดี
+Plumeria, Frangipani
+
+เฟื่องฟ้า
+Bougainvillea "Imperial Thai Delight"
+
+
+Heliconia rostrata
+
+
+Zingiber spectabile, Beehive Ginger
+
+
+
+
+
+beehive-ginger.jpg
+bougainvillea.jpg
+flame-tree.jpg
+golden-shower-tree.jpg
+heliconia-rostrata.jpg
+jasmine.jpg
+lotus.jpg
+orchid.jpg
+plumeria.jpg
+sakura.jpg
+
+
+
+
+
+
