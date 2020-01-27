83f8cdc257144209e52ce3019eb87930363bbd18
voyccom <john@hagstrand.com>
2020-01-27 05:25:51 -0700

    sectioning lessons

---
+++ html/thai/course/au/adj.js
@@ -0,0 +1,29 @@
+https://www.thaipod101.com/lesson/absolute-beginner-thai-for-every-day-4-top-25-thai-adjectives/
+
+ซ้าย (sáai) Left.
+ขวา (khwăa) Right.
+ดี (dii) Good.
+แย่(yâae) Bad.
+ร้อน( ráawn) Hot
+เย็น (yen) Cold.
+สวย (sŭuai) beautiful.
+น่าเกลียด (nâa-glìiat) ugly.
+เผ็ด (phèt) Spicy.
+อร่อย (a-ràuy) Tasty.
+ถูก (thùuk) cheap.
+แพง (phaaeng) expensive.
+ดีใจ (dii-jai) Happy.
+เสียใจ (sĭia-jai) sad.
+ใหญ่ (yài) big, huge, large.
+เล็ก (lék) small.
+เหมือน (mǔuean) same.
+สำคัญ (săm-khan) Important.
+เร็ว (reo) Fast.
+ช้า(cháa) Slow.
+สะอาด (sà-àat) Clean.
+สกปรก (sòk-gà-bpròk) Dirty.
+น่ารัก (nâa-rák) Cute.
+ง่าย (ngâai) Easy.
+
+
+มาก (mâak) very
