bed57b7cfdb12856cea102fe941a5357473c50fb
voyccom <john@hagstrand.com>
2020-05-28 00:15:51 -0700

    add backup to cron

--- cli/scripts/dbbackup.sh
+++ cli/scripts/dbbackup.sh
@@ -1,10 +1,8 @@
-# 
-#  table thaidict is static
-#  tables vocab and profile are dynamically updated by users
 #
 #  -Fp format plain-text SQL script file
 #  -b  include blobs
 #  -O  do NOT output commands to alter ownership of objects.  User running the command will have ownership of all the objects.
 #  --column-inserts  dump data as insert statements with explicit column names
 #
-pg_dump -Fp -b -O --column-inserts -U voyccom_jhagstrand -n mai -f /home/voyccom/db_backup/mai-backup.`date +%Y%m%d`.`date +%H%M%S`.sql voyccom_mai
+pg_dump -Fp -b -O --column-inserts -U voyccom_jhagstrand -n mai -n account -f /home/voyccom/db_backup/mai-backup.`date +%Y%m%d`.`date +%H%M%S`.sql voyccom_mai
+echo "mai.voyc backup completed"
